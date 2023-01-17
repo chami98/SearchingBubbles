721e2bae499ffb0c2698d4d02bb36d0cae6a191b
chami98 <chamikara98@gmail.com>
2023-01-17 22:25:54 +0530

    Refactored the code with es6

--- algo.js
+++ algo.js
@@ -28,38 +28,38 @@ let arr = [234, 43, 55, 63, 5, 6, 235, 547];
 // Now pass this array to the bubbleSort() function
 bubbleSort(arr);
 
-function swap(arr, xp, yp) {
-    var temp = arr[xp];
+function swap(arr,xp, yp)
+{
+    let temp = arr[xp];
     arr[xp] = arr[yp];
     arr[yp] = temp;
 }
-
-function selectionSort(arr, n) {
-    var i, j, min_idx;
-
+ 
+const selectionSort =(arr,  n)=>
+{
+    let i, j, min_idx;
+ 
     // One by one move boundary of unsorted subarray
-    for (i = 0; i < n - 1; i++) {
+    for (i = 0; i < n-1; i++)
+    {
         // Find the minimum element in unsorted array
         min_idx = i;
         for (j = i + 1; j < n; j++)
-            if (arr[j] < arr[min_idx])
-                min_idx = j;
-
+        if (arr[j] < arr[min_idx])
+            min_idx = j;
+ 
         // Swap the found minimum element with the first element
-        swap(arr, min_idx, i);
+        swap(arr,min_idx, i);
     }
 }
-
-function printArray(arr, size) {
-    var i;
-    for (i = 0; i < size; i++)
-        document.write(arr[i] + " ");
-    document.write(" <br>");
+ 
+const printArray = ( arr,  size)=>
+{
+    console.log(arr);
 }
-
-var arr2 = [64, 25, 12, 22, 11];
-var n = 5;
-selectionSort(arr, n);
-document.write("Sorted array: <br>");
-printArray(arr2, n);
-
+ 
+let arr2 = [64, 25, 12, 22, 11];
+    let n = 5;
+    selectionSort(arr2, n);
+    
+    printArray(arr2, n);
