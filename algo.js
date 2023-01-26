4cda6a6115fbd1238dc4df5269f7183b08a92864
chami98 <chamikara98@gmail.com>
2023-01-26 23:19:25 +0530

    Implemented the quick sort algorithm

--- algo.js
+++ algo.js
@@ -63,7 +63,7 @@ printArray(arr2, n);
 // Javascript program for insertion sort 
 
 // Function to sort an array using insertion sort
-function insertionSort(arr, n) {
+const insertionSort = (arr, n) => {
     let i, key, j;
     for (i = 1; i < n; i++) {
         key = arr[i];
@@ -93,4 +93,81 @@ let arr1 = [12, 11, 13, 5, 6];
 let n1 = arr.length;
 
 insertionSort(arr, n);
+printArray(arr, n); \
+
+// Javascript implementation of QuickSort
+
+
+// A utility function to swap two elements
+function swap(arr, i, j) {
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+/* This function takes last element as pivot, places
+   the pivot element at its correct position in sorted
+   array, and places all smaller (smaller than pivot)
+   to left of pivot and all greater elements to right
+   of pivot */
+const partition = (arr, low, high) => {
+
+    // pivot
+    let pivot = arr[high];
+
+    // Index of smaller element and
+    // indicates the right position
+    // of pivot found so far
+    let i = (low - 1);
+
+    for (let j = low; j <= high - 1; j++) {
+
+        // If current element is smaller
+        // than the pivot
+        if (arr[j] < pivot) {
+
+            // Increment index of
+            // smaller element
+            i++;
+            swap(arr, i, j);
+        }
+    }
+    swap(arr, i + 1, high);
+    return (i + 1);
+}
+
+/* The main function that implements QuickSort
+          arr[] --> Array to be sorted,
+          low --> Starting index,
+          high --> Ending index
+ */
+const quickSort = (arr, low, high) => {
+    if (low < high) {
+
+        // pi is partitioning index, arr[p]
+        // is now at right place
+        let pi = partition(arr, low, high);
+
+        // Separately sort elements before
+        // partition and after partition
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+// Function to print an array
+const printArray = (arr, size) => {
+    for (let i = 0; i < size; i++)
+        document.write(arr[i] + " ");
+
+    document.write("<br>");
+}
+
+// Driver Code
+
+let arr3 = [10, 7, 8, 9, 1, 5];
+let n2 = arr.length;
+
+quickSort(arr, 0, n - 1);
+document.write("Sorted array: <br>");
 printArray(arr, n);
